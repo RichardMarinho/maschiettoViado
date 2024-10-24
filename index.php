5e7594ba1a131cc5bf6b9e5a4fe3ab116dcf6af8
Gusmaozz <gusmaog866@gmail.com>
2024-10-24 11:31:18 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+<body>
+
+<header>
+        <!--Dentro desse <header> está a barra de navegação da página, adequada para desktop e mobile -->
+        <nav class="nav-bar"> <!--Código usado para a exibição se enquadrar aos computadores -->
+
+            <div class="nav-list">
+                <ul>
+                    <li class="nav-item"><a href="index.php" class="nav-link">Jogos</a></li>
+                    <li class="nav-item"><a href="carrinho.php" class="nav-link">Carrinho</a></li>
+                    <li class="nav-item"><a href="cosmeticos
+                </ul>
+            </div>
+            <div class="login-button">
+                <button><a href="cadastro.php">Entrar</a></button>
+            </div> <!--Fim do código usado para a exibição em computadores -->
+
+         
+        </div><!--Fim do código usado para a exibição em celulares/tablets -->
+    </header> <!--Fim do Header -->
+
+
+</body>
+</html>
